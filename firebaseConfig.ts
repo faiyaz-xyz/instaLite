4746a1023d9d5f02805dce945ed555223f6a9d2b
Ayman Tahiat Faiyaz <tahiat0967@gmail.com>
2025-08-11 09:49:12 +0600

    setup complete, working on auth

---
+++ firebaseConfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaOw54JeM2a5U1PloCVXOJ9wsUpqpT9zk",
+  authDomain: "insta-lite-c1edf.firebaseapp.com",
+  projectId: "insta-lite-c1edf",
+  storageBucket: "insta-lite-c1edf.firebasestorage.app",
+  messagingSenderId: "631055723149",
+  appId: "1:631055723149:web:dfe82a1119256b9493d0c4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
